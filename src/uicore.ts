700669ccdf77dea0819bdd1fb1ba8e31d27ca469
Tch1b0 <Jpour3006@gmail.com>
2022-07-24 11:51:48 +0200

    remove redundant event emitter

--- src/uicore.ts
+++ src/uicore.ts
@@ -2,21 +2,13 @@ import { EventEmitter } from "node:events";
 import { UiComponent } from ".";
 import { PageConfig } from "./pageconfig";
 
-export declare interface UiCore {
-    on(event: "preRender", listener: (element: HTMLElement) => void): this;
-    on(event: "postRender", listener: (element: HTMLElement) => void): this;
-    on(event: string, listener: Function): this;
-}
-
-export class UiCore extends EventEmitter {
+export class UiCore {
     element!: HTMLElement;
 
     constructor(
         public template: UiComponent,
         public pageConfig: PageConfig = new PageConfig()
-    ) {
-        super();
-    }
+    ) {}
 
     render(target: string | HTMLElement = this.element) {
         if (typeof target === "string") {
@@ -25,15 +17,11 @@ export class UiCore extends EventEmitter {
             this.element = target;
         }
 
-        this.emit("preRender", this.element);
         this.element.innerHTML = this.template();
-        this.emit("postRender", this.element);
     }
 
     plainRender(asPage: boolean = true): string {
-        this.emit("preRender", this.element);
         let rendered = this.template();
-        this.emit("postRender", this.element);
         if (asPage) {
             this.pageConfig.applyConfig();
             rendered = this.pageConfig.apply("UI_ROOT", rendered);
